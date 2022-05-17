61a88eb16822e3d7f3bfddf050dbc84e242974d0
Hector Bonilla <dc5trust@gmail.com>
2022-05-16 20:25:35 -0700

    mobile menu pop-up added

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const darkModeBtn = document.querySelector('.far');
 const nav = document.querySelector('.nav-container');
 
+const dotsContainer = document.querySelector('.dots-container');
+const mobileMenu = document.querySelector('.mobile-nav-popup');
+const mobileLinks = document.querySelectorAll('.mobile-nav-popup li');
+const xBtnMobileMenu = document.querySelector('.x-btn');
 
+dotsContainer.addEventListener('click', openMobileMenu);
+xBtnMobileMenu.addEventListener('click', closeMobileMenu);
+
+function openMobileMenu(){
+    mobileMenu.style.display = 'flex';
+    nav.setAttribute('style', 'display: none');
+}
+
+mobileLinks.forEach((link)=>{
+    link.addEventListener('click', openMobileLinks)
+});
+
+function openMobileLinks(e){
+    mobileMenu.style.display = 'none'
+    nav.setAttribute('style', 'display: flex');
+
+}
+
+function closeMobileMenu(){
+    mobileMenu.setAttribute('style', 'display: none');
+    nav.setAttribute('style', 'display: flex');
+}
 //global variables 
-let darkModeSwitch = false;
+// let darkModeSwitch = false;
 
 // darkModeBtn.addEventListener('click', (e)=>{
 //     //Switch Background Color to Black
@@ -25,3 +51,4 @@ let darkModeSwitch = false;
 //     }
     
 // });
+
