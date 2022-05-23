cc44a958968fc6ed6f55803b6edf63b69bd697a0
Hector Bonilla <dc5trust@gmail.com>
2022-05-23 16:26:53 -0700

    added youtube links and refactored some code

--- app.js
+++ app.js
@@ -28,27 +28,6 @@ function closeMobileMenu(){
     mobileMenu.setAttribute('style', 'display: none');
     nav.setAttribute('style', 'display: flex');
 }
-//global variables 
-// let darkModeSwitch = false;
 
-// darkModeBtn.addEventListener('click', (e)=>{
-//     //Switch Background Color to Black
-//     document.body.classList.toggle('dark-mode-background');
-    
-//     //check if Dark Mode is ON, 'false' or 'true'  will triggler the appropriate Shadow 
-//     if(!darkModeSwitch){
-//         //switch icon; from the Moon to the Sun 
-//         darkModeBtn.classList.remove('fa-moon');
-//         darkModeBtn.classList.add('fa-sun');
-//         nav.style.boxShadow = '0 15px 25px rgba(255, 255, 255, 0.07), 0 4px 4px 0 rgba(255, 255, 255, 0.05)';
-//         darkModeSwitch = !darkModeSwitch;
-//     }else if(darkModeSwitch){
-//         //switch icon; from the Sun to the Moon 
-//         darkModeBtn.classList.add('fa-moon');
-//         darkModeBtn.classList.remove('fa-sun');
-//         nav.style.boxShadow = '0 15px 25px rgba(0, 0, 0, 0.07), 0 4px 4px 0 rgba(0, 0, 0, 0.05)';
-//         darkModeSwitch = !darkModeSwitch;
-//     }
-    
-// });
+
 
